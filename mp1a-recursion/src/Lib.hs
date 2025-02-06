e89121a1387c7826e43293b0a7dfadb5b4aa5f6e
hanpuliu-charles <26217378+hanpuliu-charles@users.noreply.github.com>
2025-02-06 12:38:50 -0600

    fib

--- mp1a-recursion/src/Lib.hs
+++ mp1a-recursion/src/Lib.hs
@@ -96,12 +96,15 @@ ones = 1:ones
 --- ### nats
 
 -- don't forget to put the type declaration or you will lose points!
-nats = undefined
+nats :: [Integer]
+nats = [1..]
 
 --- ### fib
 
 -- don't forget to put the type declaration or you will lose points!
-fib = undefined
+-- credits: https://wiki.haskell.org/The_Fibonacci_sequence
+fib :: [Integer]
+fib = 0 : 1 : addpairs fib (tail fib)
 
 --- Set Theory
 --- ----------
