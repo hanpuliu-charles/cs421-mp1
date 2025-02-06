b8fdaf081b01cf8ec1c35297f48a5c88d71a8b96
hanpuliu-charles <26217378+hanpuliu-charles@users.noreply.github.com>
2025-02-06 13:57:13 -0600

    implement intersect function for list intersection

--- mp1a-recursion/src/Lib.hs
+++ mp1a-recursion/src/Lib.hs
@@ -132,8 +132,13 @@ union (x:xs) (y:ys)
 --- ### intersect
 
 -- don't forget to put the type declaration or you will lose points!
-intersect = undefined
-
+intersect :: Ord a => [a] -> [a] -> [a] 
+intersect x [] = []
+intersect [] y = []
+intersect (x:xs) (y:ys) 
+    | x == y = x:intersect xs ys
+    | x < y  = intersect xs (y:ys)
+    | otherwise = intersect (x:xs) ys
 --- ### powerset
 
 -- don't forget to put the type declaration or you will lose points!
